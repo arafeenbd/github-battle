123313557d5d26d82a0a9f91f8a3fa89a8d7bd56
Junaid Arafeen <arafeenbd@yahoo.com>
2017-05-03 18:26:27 -0500

    initial commit for popular

--- app/index.js
+++ app/index.js
@@ -1,16 +1,7 @@
 var React = require('react')
 var ReactDom = require('react-dom')
 require('./index.css')
-
-class App extends React.Componnet {
-  render() {
-    return (
-      <div>
-        Hello World
-      </div>
-    )
-  }
-}
+var App = require('./components/App')
 
 ReactDom.render(
   <App />,
