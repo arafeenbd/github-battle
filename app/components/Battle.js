f6c38c5ee8ec22c7aba0d1dfe8f09caa76b6127a
Junaid Arafeen <junaid.arafeen@bestbuy.com>
2017-05-08 09:14:12 -0500

    initial PlayerPreview page

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -66,12 +66,13 @@ class Battle extends React.Component {
     super(props)
     this.state = {
       playerOne:'',
-      playerOneImage:'',
+      playerOneImage:null,
       playerTwo:'',
-      playerTwoImage:''
+      playerTwoImage:null
     }
 
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleReset = this.handleReset.bind(this)
   }
 
   handleSubmit(id, username) {
@@ -81,27 +82,79 @@ class Battle extends React.Component {
         newState[id + 'Image'] = 'https://github.com/' + username + '.png?size'
         return newState
     })
-
   }
+
+  handleReset(id) {
+    this.setState(function() {
+      var newState = {}
+      newState[id] = ''
+      newState[id+'Image'] = null
+      return newState
+    })
+  }
+
   render() {
     var playerOne = this.state.playerOne
     var playerTwo = this.state.playerTwo
+    var playerOneImage = this.state.playerOneImage
+    var playerTwoImage = this.state.playerTwoImage
 
     return (
       <div>
-      {!playerOne &&
-        <PlayerInput
-          id='playerOne'
-          label='Player One'
-          onSubmit={this.handleSubmit}/>}
-      {!playerTwo &&
-        <PlayerInput
-          id='playerTwo'
-          label='Player Two'
-          onSubmit={this.handleSubmit} />}
+        {!playerOne &&
+          <PlayerInput
+            id='playerOne'
+            label='Player One'
+            onSubmit={this.handleSubmit}/>}
+
+        {
+          playerOneImage !== null &&
+          <PlayerPreview
+            username={playerOne}
+            image={playerOneImage}
+            onReset={this.handleReset}/>
+        }
+
+        {!playerTwoImage &&
+          <PlayerInput
+            id='playerTwo'
+            label='Player Two'
+            onSubmit={this.handleSubmit} />}
+
+        {
+          playerTwoImage !== null &&
+          <PlayerPreview
+            username={playerTwo}
+            image={playerTwoImage}
+            onReset={this.handleReset}/>
+        }
       </div>
     )
   }
+}
+
+function PlayerPreview(props) {
+  return (
+    <div>
+      <div className='column'>
+        <img
+          className='avatar'
+          src={props.image}
+          alt={'Avator for' + props.username}
+        />
+        <h2 className='username'>@{props.username}</h2>
+      </div>
+      <button
+        className='reset'>
+          Reset
+      </button>
+    </div>
+  )
+}
+
+PlayerPreview.propTypes = {
+  username : PropTypes.string.isRequired,
+  image : PropTypes.string.isRequired
 }
 
 module.exports = Battle
